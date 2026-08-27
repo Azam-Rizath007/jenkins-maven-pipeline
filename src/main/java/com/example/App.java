2ed80309234e8d2038b182d3383e426bcfd77940
Azam_Rizath <rizathazaam@gmail.com>
2026-08-27 13:15:36 +0530

    Checking automatic biuld

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,6 +3,6 @@ package com.example ;
 public class App {
   public static void main (String [] args){
     System.out.println("Hello from the Jenkins Maven Pipeline!");
-    System.out.println("The Maven build completed successfully.");
+    System.out.println("GitHub webhook triggered Jenkins successfully!");
   }
 }
