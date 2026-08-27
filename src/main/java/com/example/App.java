97805b02166fb92060814cb03580c326da636808
Azam_Rizath <rizathazaam@gmail.com>
2026-08-27 11:59:23 +0530

    Add java Application

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,8 @@
+package com.example ;
+
+public class App {
+  public static void main (String [] args){
+    System.out.println("Hello from the Jenkins Maven Pipeline!");
+    System.out.println("The Maven build completed successfully.");
+  }
+}
